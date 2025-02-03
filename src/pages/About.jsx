457b92ce5955f41ea9c36ac79f4946fb0d6a4c64
arnoldson <arnold.son@icloud.com>
2025-02-03 14:39:04 -0800

    Added an 'About Me' page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,16 @@
 export function About() {
-  return <h1>ABOUT ME</h1>
+  return (
+    <div className="section">
+      <img className="section-img" src="./img/IMG_0796.JPG" alt="" />
+      <h1 className="section-header">ABOUT ME</h1>
+      <p className="section-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam a quia
+        neque est molestiae quod quidem impedit dolores ea error. Debitis
+        consequuntur voluptate nihil vero voluptates laudantium nobis quidem
+        nemo tempora dolor aperiam libero dolore quibusdam pariatur consequatur
+        tempore, repellat accusamus obcaecati ratione molestias. Quidem
+        quibusdam temporibus sapiente eveniet nulla?
+      </p>
+    </div>
+  )
 }
