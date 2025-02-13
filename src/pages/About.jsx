0b6524b8ad5d90b1f59bf01046f8e127327c1e00
arnoldson <arnold.son@icloud.com>
2025-02-12 18:20:15 -0800

    updated about me page with new photo and writeup.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,14 @@
 export function About() {
   return (
     <div className="section">
-      <img className="section-img" src="./img/IMG_0796.JPG" alt="" />
+      <img className="section-img" src="./img/IMG_5012.JPG" alt="" />
       <h1 className="section-header">ABOUT ME</h1>
       <p className="section-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam a quia
-        neque est molestiae quod quidem impedit dolores ea error. Debitis
-        consequuntur voluptate nihil vero voluptates laudantium nobis quidem
-        nemo tempora dolor aperiam libero dolore quibusdam pariatur consequatur
-        tempore, repellat accusamus obcaecati ratione molestias. Quidem
-        quibusdam temporibus sapiente eveniet nulla?
+        Passionate about crafting seamless web experiences, I’m a web developer
+        with a love for clean code and innovative design. When I’m not building
+        websites, you’ll find me scaling new heights—both in coding challenges
+        and on climbing walls. I thrive on problem-solving, adventure, and
+        pushing limits in both worlds.
       </p>
     </div>
   )
