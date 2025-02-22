1b303f3c28d4ece3b8a85c0ef6bc5da1004d8dcb
arnoldson <arnold.son@icloud.com>
2025-02-22 12:06:26 -0800

    added hamburger menu on mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom"
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded"
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded"
+import { useState } from "react"
 
 export function Navbar() {
+  const [isMenuCollapsed, setIsMenuCollapsed] = useState(true)
   return (
     <nav className="nav">
       <span className="nav-title">Arnold Son</span>
-      <ul className="link-list">
+      <button
+        className="menu-button"
+        onClick={() => setIsMenuCollapsed(!isMenuCollapsed)}
+      >
+        {isMenuCollapsed ? <MenuRoundedIcon /> : <CloseRoundedIcon />}
+      </button>
+
+      <ul className={`link-list ${isMenuCollapsed ? "collapsed" : ""}`}>
         <li>
           <Link className="nav-link" to="about">
             About
